e671388c91c2a3181254b0dd5880c8adc55fe4e8
CyrilSbrodov <sbrandy@bk.ru>
2023-12-02 19:24:37 +0300

    add docker env

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -23,7 +23,7 @@ func NewServerConfig() *ServerConfig {
 	cfgSrv := &ServerConfig{}
 	flag.StringVar(&cfgSrv.Addr, "a", "localhost:8080", "ADDRESS")
 	flag.StringVar(&cfgSrv.ExternalAddr, "e", "https://probe.fbrq.cloud/v1/send/", "external address gor send mailing")
-	flag.StringVar(&cfgSrv.DatabaseDSN, "d", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable", "DATABASE_DSN")
+	flag.StringVar(&cfgSrv.DatabaseDSN, "d", "postgres://postgres:postgres@postgres:5432/postgres?sslmode=disable", "DATABASE_DSN")
 	flag.StringVar(&cfgSrv.Token, "token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzI3OTk0MzksImlzcyI6ImZhYnJpcXVlIiwibmFtZSI6Imh0dHBzOi8vdC5tZS9jeXJpbHNicm9kb3YifQ.UEIeNXd517YYWYMWjKoRXzYI0VuspTR8irWd82Bb1qM", "JWT Token for external addr")
 	flag.DurationVar(&cfgSrv.CheckInterval, "c", 5*time.Second, "time interval for checking a new mailing list")
 	flag.Parse()
